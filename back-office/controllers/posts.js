64443efb7a72c51faa2fd82bc05e8b5aad242f83
HiImAlex-hub <alexandru.nicolescu06@gmail.com>
2022-11-01 20:21:13 +0100

    API users, posts, products DONE

--- back-office/controllers/posts.js
+++ back-office/controllers/posts.js
@@ -2,8 +2,37 @@ const Post = require("../models/posts");
 const { createCustomError } = require("../errors/custom-error");
 
 const getAllPosts = async (req, res) => {
-  const posts = await Post.find({});
-  res.status(200).json({ posts });
+  const { title, text, sort, fields } = req.query;
+  const queryObject = {};
+
+  if (title) {
+    queryObject.title = { $regex: title, $options: "i" };
+  }
+  if (text) {
+    queryObject.text = { $regex: text, $options: "i" };
+  }
+
+  let result = Post.find(queryObject);
+  // sort
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("date");
+  }
+
+  if (fields) {
+    const fieldList = fields.split(",").join(" ");
+    result = result.select(fieldList);
+  }
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  // calcolo i risultati da saltare in base alla pagina che mi trovo
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+  const posts = await result;
+  res.status(200).json(posts);
 };
 
 const createPost = async (req, res) => {
